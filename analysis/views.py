78cb2cae2b2a60d74333624bd6a642895b912f59
abhishekpythons <abhishekpythons@gmail.com>
2025-08-01 17:23:16 +0530

    configuring homepage and media server in analysis app

--- analysis/views.py
+++ analysis/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def analysis_view(request):
     return render(request, 'analysis.html')
+
+def analyze(request, shapefile_path):
+    # Calculate the output.jpg, save it at MEDIA_ROOT
+    return "output.jpg"
